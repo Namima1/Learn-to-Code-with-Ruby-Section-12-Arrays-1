aaa6d37ff48928038a1c8a76c2b29bef5d1bd856
Namima1 <129132573+Namima1@users.noreply.github.com>
2023-09-11 21:14:31 -0400

    Add files via upload

---
+++ Arrays.rb
@@ -0,0 +1,11 @@
+#An array is an ordered list of objects
+#We use the word "elements" to describe items in array. 
+#array itself is an object 
+#example:a box can hold multiple things in it. The box is an object...
+#and everything in it is a object
+
+numbers = [4, 8, 15, 16, 23, 42]
+p numbers 
+
+#spacing and comma is important bc that is what is distinguishing the items
+
