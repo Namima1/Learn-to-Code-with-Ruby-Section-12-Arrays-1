0c4779ecff03a287f4cae20b302a46cba9605e94
Namima1 <129132573+Namima1@users.noreply.github.com>
2023-09-13 11:17:57 -0400

    Add files via upload

---
+++ nested_arrays_1.rb
@@ -0,0 +1,11 @@
+#nested arrays are arrays that hold other data in them 
+
+spreadsheet = [
+    ["Student", "Class", "Grade"],
+    ["Sally", "Computer Science", 95], 
+    ["Ingrid", "Physics", 45]
+]
+
+p spreadsheet 
+
+#each bracket/array is a element within larger array(spreadhseet)
