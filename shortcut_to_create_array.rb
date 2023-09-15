8b676899360b0932f397be0a4fe0d1f41113b3da
Namima1 <129132573+Namima1@users.noreply.github.com>
2023-09-15 12:44:10 -0400

    Add files via upload

---
+++ shortcut_to_create_array.rb
@@ -0,0 +1,8 @@
+names = ["Jack, "Jill", "John", "James"]
+p names 
+
+names = %w[Jack Jill John James]
+
+# %w is a shortcut where you don't need to add commas or quotes, it adds for you
+
+p names 
