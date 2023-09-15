ac374cf5774fa4b45348144684c8d2ce83451f14
Namima1 <129132573+Namima1@users.noreply.github.com>
2023-09-15 13:13:20 -0400

    Add files via upload

---
+++ overwrite_arrays_1.rb
@@ -0,0 +1,8 @@
+fruits = ["Apples", "Orange", "Grape", "Banana"]
+
+#in a array, each string has a index position
+#index positon of apple is 0, ip of banana is 4 
+
+puts fruits[0]
+puts fruits [1]
+puts fruits [3]
