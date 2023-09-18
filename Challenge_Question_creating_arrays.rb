9cb6cd853572ba8d6a3af858078df7a5a4d082d1
Namima1 <129132573+Namima1@users.noreply.github.com>
2023-09-17 22:00:36 -0400

    Add files via upload

---
+++ Challenge_Question_creating_arrays.rb
@@ -0,0 +1,15 @@
+#Declare an empty_array method that return an empty array 
+def empty_array 
+  []
+end
+
+p empty_array
+
+#declare a three_colors method that returns an array with
+#the strings of "red", "green", "blue (in that order)"
+
+def three_colors
+  %w[red green blue]
+end 
+
+p three_colors
