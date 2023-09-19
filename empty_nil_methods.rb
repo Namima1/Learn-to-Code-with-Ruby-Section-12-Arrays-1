80f0cbcc5f3e615f5802a59030a00637da20ee73
Namima1 <129132573+Namima1@users.noreply.github.com>
2023-09-18 21:57:47 -0400

    Add files via upload

---
+++ empty_nil_methods.rb
@@ -0,0 +1,15 @@
+#empty checks if array is 0 
+
+puts ["A", "B"].empty? 
+
+#empty is written with a question mark 
+
+puts [].empty? 
+
+
+puts [].nil? 
+#nil is not used in arrays 
+
+puts nil.nil? 
+
+#this checks if something will be nil 
