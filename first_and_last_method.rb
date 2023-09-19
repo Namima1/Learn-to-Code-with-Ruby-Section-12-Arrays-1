25ece012bd566c7659c9dffe3d8e439a65a9eb02
Namima1 <129132573+Namima1@users.noreply.github.com>
2023-09-18 21:39:47 -0400

    Add files via upload

---
+++ first_and_last_method.rb
@@ -0,0 +1,11 @@
+#allows us to extract number of elements from beginning and end of array 
+
+sushi = ["Salmon", "Tuna", "Shrimp", "Eel", "Yellowtail"]
+
+p sushi.first
+#this gives me the first element in the array
+p sushi.first(4)
+
+p sushi.last
+#this gives me the last element, no number is necessary 
+p sushi.last(2)
