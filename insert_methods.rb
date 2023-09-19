2693606a280b76d77589030310f545784bc641a0
Namima1 <129132573+Namima1@users.noreply.github.com>
2023-09-19 15:33:16 -0400

    Add files via upload

---
+++ insert_methods.rb
@@ -0,0 +1,7 @@
+credit_card_transactions = [19.99, 29.43, 3.87]
+
+credit_card_transactions.insert(1, 49.99)
+p credit_card_transactions
+
+#the 1 in the parenthesis is identifying the index position
+#the 49.99 is saying what value to add 
