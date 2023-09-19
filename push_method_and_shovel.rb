708925d3800cbdf66607e42ab5257eabdf9500e7
Namima1 <129132573+Namima1@users.noreply.github.com>
2023-09-19 15:27:06 -0400

    Add files via upload

---
+++ push_method_and_shovel.rb
@@ -0,0 +1,16 @@
+soups = ["French Onion", "Clam Chowder", "Chicken Noodle"]
+
+soups.push("Miso")
+#push will add element to the end of array 
+p soups 
+
+soups.push("Wonton", "Hot and Sour")
+p soups 
+
+#push sends all elements of array to the end of previous array
+
+locations = ["Airport", "Bar", "Saloon"]
+locations << "House"
+p locations
+
+#house will now be added to the end of array 
